e12ed9870cabfe71d01de9b960561980bcd7ba4e
Bateyjosue <josuebatey19@gmail.com>
2023-01-17 21:43:51 +0200

    Add style to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user, class_name: "User", foreign_key: "user_id"
+  has_many :transactions
 
   validates :name, presence: true
   validates :user_id, presence: true
