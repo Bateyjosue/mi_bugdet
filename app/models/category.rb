e5ae3f9203312c79e37663f6c8b8062650d5a866
Bateyjosue <josuebatey19@gmail.com>
2023-01-17 16:16:13 +0200

    Add category index and new Actions

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,12 @@
 class Category < ApplicationRecord
-  belongs_to :user, class_name: "users", foreign_key: "user_id"
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
 
   validates :name, presence: true
   validates :user_id, presence: true
   validates :user_id, presence: true
   validates :image, presence: true
+
+  def total_pric
+    
+  end
 end
