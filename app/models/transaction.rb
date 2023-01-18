779595f18c5f5b7af0c29290dfc6ef0bfee1a757
Bateyjosue <josuebatey19@gmail.com>
2023-01-18 08:00:15 +0200

    Add transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,13 @@
 class Transaction < ApplicationRecord
-  belongs_to :transaction, class_name: "Transaction", foreign_key: "category_id"
+  belongs_to :category, class_name: "Category", foreign_key: "category_id"
+
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+
+  def trans_total(category)
+    total_price = Transaction.group(category).sun(:price)
+    total_price
+  end
 end
