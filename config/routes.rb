5c00c11cfa1b948996ffccf2c40966125b8f4106
Bateyjosue <josuebatey19@gmail.com>
2023-01-18 08:04:01 +0200

    Add trasnsaction routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # get 'transactions/index'
+  # get 'transactions/new'
+  # get 'transactions/create'
   get 'splansh/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,5 +9,20 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "splansh#index"
 
-  resources :categories
+  resources :categories do
+    resources :transactions, only: [:index, :new, :create]
+  end
 end
+
+
+# new_category_transaction GET    /categories/:category_id/transaction/new(:format)    transactions#new
+# edit_category_transaction GET    /categories/:category_id/transaction/edit(.:format) transactions#edit
+# category_transaction GET    /categories/:category_id/transaction(.:format) transactions#show
+# PATCH  /categories/:category_id/transaction(.:format)transactions#update
+# PUT    /categories/:category_id/transaction(.:format)transactions#update
+# DELETE /categories/:category_id/transaction(.:format) transactions#destroy
+# POST   /categories/:category_id/transaction(.:format) transactions#create
+
+# category_transactions GET    /categories/:category_id/transactions(.:format)transactions#index
+# POST   /categories/:category_id/transactions(.:format)transactions#create
+# new_category_transaction GET    /categories/:category_id/transactions/new(.:format)transactions#new
