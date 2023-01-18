98698a6429e3d2a7ab8a920ac9ebd35ceee20e8b
Bateyjosue <josuebatey19@gmail.com>
2023-01-18 10:27:24 +0200

    Add Navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,3 @@ Rails.application.routes.draw do
     resources :transactions, only: [:index, :new, :create]
   end
 end
-
-
-# new_category_transaction GET    /categories/:category_id/transaction/new(:format)    transactions#new
-# edit_category_transaction GET    /categories/:category_id/transaction/edit(.:format) transactions#edit
-# category_transaction GET    /categories/:category_id/transaction(.:format) transactions#show
-# PATCH  /categories/:category_id/transaction(.:format)transactions#update
-# PUT    /categories/:category_id/transaction(.:format)transactions#update
-# DELETE /categories/:category_id/transaction(.:format) transactions#destroy
-# POST   /categories/:category_id/transaction(.:format) transactions#create
-
-# category_transactions GET    /categories/:category_id/transactions(.:format)transactions#index
-# POST   /categories/:category_id/transactions(.:format)transactions#create
-# new_category_transaction GET    /categories/:category_id/transactions/new(.:format)transactions#new
