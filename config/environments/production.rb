07cc11fa3899cae962afc31cdd951c934377c19f
Bateyjosue <josuebatey19@gmail.com>
2023-01-19 16:21:36 +0200

    configure letter opener in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mi_bugdet_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://mi-budget.onrender.com/', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
