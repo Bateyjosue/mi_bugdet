0c720e79520953e627abcea1b9810866f4a908d0
Josh Batey <josuebatey19@gmail.com>
2023-01-20 23:56:21 +0200

    Apply review fix Rubocop

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,24 +1,10 @@
-# rubocop:disable Layout/LineLength
-FactoryBot.define do
-  factory :transaction do
-    name { 'Dinner' }
-    price { 1100.2 }
-    description { 'food dinner 1' }
-    image {'https://www.eatthis.com/wp-content/uploads/sites/4/2017/12/waffle-honey-yogurt-bananas-flickr.jpg?quality=82&strip=1'}
-  end
 
+FactoryBot.define do
+# rubocop:disable Layout/LineLength
   factory :user do
     name { 'josh' }
     email { 'josuebatey19@gmail.com' }
     password { '000000' }
   end
-
-  factory :category do
-    id { 1 }
-    name { 'Dinner' }
-    description { 'food dinner 1' }
-    image {'https://www.eatthis.com/wp-content/uploads/sites/4/2017/12/waffle-honey-yogurt-bananas-flickr.jpg?quality=82&strip=1'}
-  end
+  # rubocop:enable Layout/LineLength
 end
-
-# rubocop:enable Layout/LineLength
