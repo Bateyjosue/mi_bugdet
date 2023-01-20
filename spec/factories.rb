df8499411cd0f724ad46acd491030b279a2a02d6
Josh Batey <josuebatey19@gmail.com>
2023-01-21 00:00:30 +0200

    Fixed rubocop i factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,7 @@
 FactoryBot.define do
-# rubocop:disable Metrics/LineLength
   factory :user do
     name { 'josh' }
     email { 'josuebatey19@gmail.com' }
     password { '000000' }
   end
-  # rubocop:enable Metrics/LineLength
 end
